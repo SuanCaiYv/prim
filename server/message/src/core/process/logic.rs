3f5ae7a4467b9e82d58d1343be2ff90ab0202b17
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-08-11 21:33:07 +0800

    0811

---
+++ server/message/src/core/process/logic.rs
@@ -0,0 +1,60 @@
+use tracing::{debug, info, warn, error};
+use serde::{Deserialize, Serialize};
+use crate::entity::msg;
+use crate::util::base;
+use crate::core::net;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SyncStruct {
+    // seq_num
+    s: u64,
+    // is_backing
+    b: bool,
+    // length
+    l: usize,
+}
+
+pub async fn process(msg: &msg::Msg, redis_ops: &mut net::RedisOps) -> std::io::Result<Vec<msg::Msg>> {
+    match msg.head.typ {
+        msg::Type::Sync => {
+            let params: serde_json::Result<SyncStruct> = serde_json::from_slice(msg.payload.as_slice());
+            if let Err(e) = params {
+                error!("parse params failed: {}", e);
+                let mut result = Vec::with_capacity(1);
+                result.push(msg::Msg::err_msg_str(0, msg.head.sender, "parse params failed"));
+                return Ok(result);
+            }
+            let mut params = params.unwrap();
+            // 构建key
+            let mut key = base::who_we_are(msg.head.sender, msg.head.receiver);
+            key.push_str("-msg_channel");
+            // 默认情况
+            if params.s == 0 {
+                params.s = u64::MAX;
+                params.b = true;
+                params.l = 20;
+            }
+            let list: redis::RedisResult<Vec<msg::Msg>> = redis_ops.peek_sort_queue_more(key, 0, params.l, params.b, params.s as f64).await;
+            if let Err(e) = list {
+                error!("redis read error: {}", e);
+                let mut result = Vec::with_capacity(1);
+                result.push(msg::Msg::err_msg_str(0, msg.head.sender, "redis read error"));
+                return Ok(result);
+            }
+            let mut list = list.unwrap();
+            // 结果列表
+            let mut result: Vec<msg::Msg> = Vec::with_capacity(params.l + 1);
+            result.push(msg::Msg::text(0, msg.head.sender, list.len().to_string()));
+            result.append(& mut list);
+            Ok(result)
+        }
+        msg::Type::Auth => {
+            let mut result = Vec::with_capacity(1);
+            result.push(msg::Msg::text_str(0, msg.head.sender, "ok"));
+            Ok(result)
+        }
+        _ => {
+            Err(std::io::Error::new(std::io::ErrorKind::Other, "unknown msg type"))
+        }
+    }
+}
--- server/message/src/core/process/mod.rs
+++ server/message/src/core/process/mod.rs
@@ -1,2 +1,3 @@
 pub mod biz;
 pub mod logic;
+pub mod heartbeat;
