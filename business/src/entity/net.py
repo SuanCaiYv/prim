a1f949e805530068f7df6763550322a5483a4b90
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-08-22 00:52:03 +0800

    增加了功能

--- business/src/entity/net.py
+++ business/src/entity/net.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+from numpy import uint16, uint64
+
+import util.base
+
 HEAD_LENGTH = 37
 
 
@@ -23,34 +27,89 @@ class Type(Enum):
     UnderReview = 14
     InternalError = 15
     # 业务部分
-    AddFriend = 16
+    FriendRelationship = 16
     SysNotification = 17
 
 
 class Head:
-    length: int
+    length: uint16
     type: Type
-    sender: int
-    receiver: int
-    timestamp: int
-    seq_num: int
-    version: int
+    sender: uint64
+    receiver: uint64
+    timestamp: uint64
+    seq_num: uint64
+    version: uint16
+
+    def __init__(self, length: uint16, type: Type, sender: uint64, receiver: uint64,
+                 timestamp: uint64, seq_num: uint64, version: uint16):
+        self.length = length
+        self.type = type
+        self.sender = sender
+        self.receiver = receiver
+        self.timestamp = timestamp
+        self.seq_num = seq_num
+        self.version = version
 
     @staticmethod
     def from_bytes(content: bytes):
-        pass
+        length = int.from_bytes(content[0:2], byteorder='big')
+        type = Type(int.from_bytes(content[2:3], byteorder='big'))
+        sender = int.from_bytes(content[3:11], byteorder='big')
+        receiver = int.from_bytes(content[11:19], byteorder='big')
+        timestamp = int.from_bytes(content[19:27], byteorder='big')
+        seq_num = int.from_bytes(content[27:35], byteorder='big')
+        version = int.from_bytes(content[35:37], byteorder='big')
+        return Head(uint16(length), type, uint64(sender), uint64(receiver), uint64(timestamp),
+                    uint64(seq_num), uint16(version))
 
-    def to_bytes(self):
-        pass
+    def to_bytes(self) -> bytes:
+        length = int(self.length)
+        type = int(self.type.value)
+        sender = int(self.sender)
+        receiver = int(self.receiver)
+        timestamp = int(self.timestamp)
+        seq_num = int(self.seq_num)
+        version = int(self.version)
+        array = bytearray(HEAD_LENGTH)
+        array[0:2] = int(length).to_bytes(2, byteorder='big')
+        array[2:3] = int(type).to_bytes(1, byteorder='big')
+        array[3:11] = int(sender).to_bytes(8, byteorder='big')
+        array[11:19] = int(receiver).to_bytes(8, byteorder='big')
+        array[19:27] = int(timestamp).to_bytes(8, byteorder='big')
+        array[27:35] = int(seq_num).to_bytes(8, byteorder='big')
+        array[35:37] = int(version).to_bytes(2, byteorder='big')
+        return bytes(array)
+
+    def __str__(self):
+        return 'length: {}, type: {}, sender: {}, receiver: {}, timestamp: {}, seq_num: {}, version: {}'.format(
+            self.length, self.type, self.sender, self.receiver, self.timestamp, self.seq_num, self.version)
 
 
 class Msg:
     head: Head
     body: bytes
 
+    def __init__(self, head: Head, body: bytes):
+        self.head = head
+        self.body = body
+
     @staticmethod
     def from_bytes(content: bytes):
-        pass
+        head = Head.from_bytes(content[0:HEAD_LENGTH])
+        body = content[HEAD_LENGTH:]
+        return Msg(head, body)
 
-    def to_bytes(self):
-        pass
+    def to_bytes(self) -> bytes:
+        array = bytearray(HEAD_LENGTH + len(self.body))
+        array[0:HEAD_LENGTH] = self.head.to_bytes()
+        array[HEAD_LENGTH:] = self.body
+        return array
+
+    @staticmethod
+    def friend_relationship(sender, receiver: int, info: str):
+        head = Head(uint16(len(info)), Type.FriendRelationship, uint64(sender), uint64(receiver),
+                    uint64(util.base.timestamp()), uint64(0), uint16(0))
+        return Msg(head, bytes(info, encoding='utf-8'))
+
+    def __str__(self):
+        return 'head: {}, body: {}'.format(self.head, self.body)
