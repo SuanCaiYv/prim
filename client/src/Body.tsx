44369ec6dd683506f46c51617a36b777dd2992e9
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-03-06 17:42:38 +0800

    增加前端实现

---
+++ client/src/Body.tsx
@@ -0,0 +1,19 @@
+import './Body.css'
+
+function Body() {
+    return (
+        <div className="body">
+            <div className="header">
+                <div className="search"></div>
+                <div className="add"></div>
+                <div className="chat"></div>
+                <div className="contacts"></div>
+                <div className="more"></div>
+            </div>
+            <div className="list"></div>
+            <div className="main"></div>
+        </div>
+    )
+}
+
+export default Body
