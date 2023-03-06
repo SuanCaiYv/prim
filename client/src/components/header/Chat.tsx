6474d95d8e0c652735c4bdf5a17a9b8d7352054e
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-03-06 23:12:25 +0800

    前端补充

---
+++ client/src/components/header/Chat.tsx
@@ -0,0 +1,9 @@
+import './Chat.css'
+
+function Chat() {
+    return (
+        <div className="Chat"/>
+    )
+}
+
+export default Chat;
---
+++ client/src/components/header/More.tsx
@@ -0,0 +1,9 @@
+import './More.css'
+
+function More() {
+    return (
+        <div className="more"/>
+    )
+}
+
+export default More;
