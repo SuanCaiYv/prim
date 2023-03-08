b4826bfb8a8c6dbdf3ad1c595cbfbbaabe204e1d
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-03-08 18:08:14 +0800

    补充前端

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Chat from "./components/chat/Chat";
 import Contacts from "./components/contacts/Contacts";
 import More from "./components/more/More";
 import './App.css'
-import { Context, GlobalContext } from "./context/GlobalContext";
+import { GlobalContext, UserMsgListItemData } from "./context/GlobalContext";
 import { ReactNode, useState } from "react";
 import { Msg } from "./entity/msg";
 import React from "react";
@@ -12,12 +12,15 @@ import { randomMsg } from "./mock/chat";
 class Props { }
 
 class State {
-    userMsgList: Array<Msg> = [];
-    msgMap: Map<string, Msg[]> = new Map();
-    contactList: Array<string> = [];
+    test: Map<number, number[]> = new Map();
+    userMsgList: Array<UserMsgListItemData> = [];
+    msgMap: Map<bigint, Msg[]> = new Map();
+    contactList: Array<any> = [];
     userId: bigint = 0n;
     userAvatar: string = "";
     userNickname: string = "prim-user";
+    currentChatMsgList: Array<Msg> = [];
+    currentChatUserId: bigint = 0n;
 }
 
 class App extends React.Component<Props, State> {
@@ -26,48 +29,91 @@ class App extends React.Component<Props, State> {
         this.state = new State();
     }
 
-    setUserMsgList = (list: Array<Msg>, ...cb: (() => void)[]) => {
-        this.setState({ userMsgList: list }, () => {
-            if (cb.length > 0) {
-                cb[0]();
-            }
+    peerId = (id1: bigint, id2: bigint) => {
+        if (this.state.userId === id1) {
+            return id2;
+        } else {
+            return id1;
+        }
+    }
+
+    setUserMsgList = (msg: Msg) => {
+        let peerId = this.peerId(msg.head.sender, msg.head.receiver);
+        let text = msg.payloadText();
+        let timestamp = msg.head.timestamp;
+        // @todo: avatar
+        let avatar = "/src/assets/avatar/default-avatar-" + peerId + ".png";
+        // @todo: nickname
+        let nickname = "prim-user-" + peerId;
+        let number = 1;
+        let list = this.state.userMsgList;
+        let newList
+        let v = list.find((item) => {
+            return item.peerId === peerId;
         });
+        if (v !== undefined) {
+            number = v.unreadNumber + 1;
+            newList = [...list.filter((item) => {
+                return item.peerId !== peerId;
+            }), new UserMsgListItemData(peerId, avatar, nickname, text, timestamp, number)]
+        } else {
+            newList = [...list, new UserMsgListItemData(peerId, avatar, nickname, text, timestamp, number)];
+        }
+        this.setState({ userMsgList: newList });
     }
 
-    setMsgMap = (map: Map<string, Msg[]>, ...cb: (() => void)[]) => {
-        this.setState({ msgMap: map }, () => {
-            if (cb.length > 0) {
-                cb[0]();
-            }
-        });
+    setMsgMap = (msg: Msg) => {
+        let peerId = this.peerId(msg.head.sender, msg.head.receiver);
+        let map = this.state.msgMap;
+        let list = map.get(peerId);
+        if (list === undefined) {
+            map.set(peerId, [msg]);
+        } else {
+            list.push(msg);
+        }
+        // @todo resort
+        if (peerId === this.state.currentChatUserId) {
+            this.setState({ currentChatMsgList: [...this.state.currentChatMsgList, msg] });
+        }
     }
 
-    setContactList = (list: Array<string>, ...cb: (() => void)[]) => {
-        this.setState({ contactList: list });
+    setUserId = (userId: bigint) => {
+        this.setState({ userId: userId });
     }
 
-    setUserId = (id: bigint, ...cb: (() => void)[]) => {
-        this.setState({ userId: id });
-    }
-
-    setUserAvatar = (avatar: string, ...cb: (() => void)[]) => {
+    setUserAvatar = (avatar: string) => {
         this.setState({ userAvatar: avatar });
     }
 
-    setUserNickname = (nickname: string, ...cb: (() => void)[]) => {
+    setUserNickname = (nickname: string) => {
         this.setState({ userNickname: nickname });
     }
 
+    setContactList = (list: Array<any>) => {
+        this.setState({ contactList: list });
+    }
+
+    setCurrentChatUserId = (userId: bigint) => {
+        let list = this.state.msgMap.get(userId)
+        if (list === undefined) {
+            list = [];
+            this.state.msgMap.set(userId, list);
+        }
+        this.setState({ currentChatMsgList: list });
+        this.setState({ currentChatUserId: userId });
+    }
+
     componentDidMount() {
-        let count = 0;
+        let count = 1;
         const f = () => {
-            if (count > 1) {
+            if (count > 20) {
                 return;
             }
             ++count;
             setTimeout(() => {
-                this.setUserMsgList([...this.state.userMsgList, randomMsg()], f);
-            }, 1000);
+                this.setUserMsgList(randomMsg());
+                f();
+            }, 2000);
         }
         f()
     }
@@ -77,17 +123,19 @@ class App extends React.Component<Props, State> {
             <div id={"root"}>
                 <GlobalContext.Provider value={{
                     userMsgList: this.state.userMsgList,
-                    msgMap: this.state.msgMap,
                     contactList: this.state.contactList,
                     userId: this.state.userId,
                     userAvatar: this.state.userAvatar,
                     userNickname: this.state.userNickname,
-                    setUserMsgList: (list: Array<Msg>, ...cb: (() => void)[]) => this.setUserMsgList(list, ...cb),
-                    setMsgMap: (map: Map<string, Msg[]>, ...cb: (() => void)[]) => this.setMsgMap(map, ...cb),
-                    setContactList: (list: Array<string>, ...cb: (() => void)[]) => this.setContactList(list, ...cb),
-                    setUserId: (id: bigint, ...cb: (() => void)[]) => this.setUserId(id, ...cb),
-                    setUserAvatar: (avatar: string, ...cb: (() => void)[]) => this.setUserAvatar(avatar, ...cb),
-                    setUserNickname: (nickname: string, ...cb: (() => void)[]) => this.setUserNickname(nickname, ...cb)
+                    currentChatMsgList: this.state.currentChatMsgList,
+                    currentChatUserId: this.state.currentChatUserId,
+                    setUserMsgList: this.setUserMsgList,
+                    setMsgMap: this.setMsgMap,
+                    setUserId: this.setUserId,
+                    setUserAvatar: this.setUserAvatar,
+                    setUserNickname: this.setUserNickname,
+                    setContactList: this.setContactList,
+                    setCurrentChatUserId: this.setCurrentChatUserId
                 }}>
                     <BrowserRouter>
                         <Routes>
