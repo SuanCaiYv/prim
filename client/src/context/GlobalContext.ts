5b827391563a4e0d1762962799ae3367cfd9d6fd
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-03-23 18:15:03 +0800

    增加前端实现

--- client/src/context/GlobalContext.ts
+++ client/src/context/GlobalContext.ts
@@ -1,36 +1,16 @@
 import React from 'react';
+import { UserMsgListItemData } from '../entity/inner';
 import { Msg } from "../entity/msg"
 
-class UserMsgListItemData {
-    peerId: bigint
-    avatar: string
-    remark: string
-    text: string
-    timestamp: bigint
-    unreadNumber: number
-    constructor(peerId: bigint, avatar: string, remark: string, text: string, timestamp: bigint, unreadNumber: number) {
-        this.peerId = peerId
-        this.avatar = avatar
-        this.remark = remark
-        this.text = text
-        this.timestamp = timestamp
-        this.unreadNumber = unreadNumber
-    }
-}
-
 class Context {
+    userId: bigint = 0n
     userMsgList: Array<UserMsgListItemData> = []
     msgMap: Map<bigint, Msg[]> = new Map()
-    contactList: Array<any> = []
-    userId: bigint = 0n
-    nodeId: number = 0
     currentChatMsgList: Array<Msg> = []
     currentChatPeerId: bigint = 0n
     unAckSet: Set<string> = new Set()
-    setContactList: (contactList: Array<any>) => void = () => { }
-    setUserId: (userId: bigint) => void = () => { }
     setCurrentChatPeerId: (userId: bigint) => void = () => { }
-    sendMsg: (msg: Msg) => Promise<void> = async () => {}
+    sendMsg: (msg: Msg) => Promise<void> = async () => { }
     setUnread: (peerId: bigint, unread: boolean) => Promise<void> = () => Promise.resolve();
     setLoginPageDirect: (f: () => void) => void = () => { }
     setup: () => Promise<void> = () => Promise.resolve();
@@ -38,7 +18,6 @@ class Context {
     clearState: () => void = () => { }
     loadMore: () => Promise<void> = () => Promise.resolve();
     removeUserMsgListItem: (peerId: bigint) => Promise<void> = async () => { }
-    // setChatPageDirect: (f: () => void) => void
     constructor() {
     }
 }
