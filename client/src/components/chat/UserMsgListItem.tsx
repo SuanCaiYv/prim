b78cf9300f46203c8fefcd3658532177169a3a4c
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-03-27 18:25:27 +0800

    增加前端实现

--- client/src/components/chat/UserMsgListItem.tsx
+++ client/src/components/chat/UserMsgListItem.tsx
@@ -38,8 +38,10 @@ class UserMsgListItem extends React.Component<Props, State> {
 
     onContextMenu = async (e: React.MouseEvent<HTMLDivElement>) => {
         e.preventDefault();
+    }
+
+    removeItem = async () => {
         let context = this.context as Context;
-        context.setCurrentChatPeerId(0n);
         await context.removeUserMsgListItem(this.props.peerId);
     }
 
@@ -49,27 +51,29 @@ class UserMsgListItem extends React.Component<Props, State> {
         const minutes = date.getMinutes().toString().padStart(2, '0');
         let time = `${hours}:${minutes}`;
         return (
-            <div className="user-msg-list-item" onClick={this.onClick} onContextMenu={this.onContextMenu}>
-                <img src={this.props.avatar} alt="" className='u-m-l-item-avatar' />
-                <div className="u-m-l-item-remark">
+            <div className="user-msg-list-item" onContextMenu={this.onContextMenu}>
+                <img src={this.props.avatar} alt="" className='u-m-l-item-avatar' onClick={this.removeItem} />
+                <div className="u-m-l-item-remark" onClick={this.onClick}>
                     {
                         this.props.remark
                     }
                 </div>
-                <div className="u-m-l-item-msg">
+                <div className="u-m-l-item-msg" onClick={this.onClick}>
                     <span>
                         {this.props.msg}
                     </span>
                 </div>
-                <div className="u-m-l-item-timestamp">
+                <div className="u-m-l-item-timestamp" onClick={this.onClick}>
                     {
                         time
                     }
                 </div>
-                <div className="u-m-l-item-number">
+                <div className="u-m-l-item-number" onClick={this.onClick}>
                     {
                         this.props.number > 0 ? (this.props.number > 99 ? <div className='number-0'>99+</div> : <div className='number-0'>{this.props.number}</div>) : ''
                     }
+                </div>
+                <div className='u-m-l-item-a'>
                 </div>
             </div>
         )
