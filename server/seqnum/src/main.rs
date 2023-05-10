87248ba25465867ce3f3484ac0d9e48ee4634d2d
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-05-10 18:03:03 +0800

    增加seqnum

--- server/seqnum/src/main.rs
+++ server/seqnum/src/main.rs
@@ -1,10 +1,13 @@
 use std::{
     io::{Read, Write},
-    time::Instant,
+    os::unix::prelude::OpenOptionsExt,
+    sync::{atomic::AtomicU64, Arc},
+    time::{Duration, Instant},
 };
 
 use ahash::AHashMap;
 use byteorder::{BigEndian, ByteOrder};
+use thread_local::ThreadLocal;
 use tokio::io::AsyncWriteExt;
 
 use crate::persistance::new_seq_num;
@@ -16,43 +19,34 @@ mod service;
 
 #[tokio::main]
 async fn main() {
-    // let mut file = tokio::fs::OpenOptions::new()
-    // .create(true)
-    //     .append(true)
-    //     .custom_flags(0o0040000)
-    //     .open("/Users/slma/RustProjects/prim/server/seqnum/test.txt")
-    //     .await
-    //     .unwrap();
-    // let t = Instant::now();
-    // let n = 100000;
-    // for i in 0..n {
-    //     file.write_all(format!("{:022}\n", i).as_bytes())
-    //         .await
-    //         .unwrap();
+    // for i in 0..8 {
+    //     std::thread::spawn(move || {
+    //         let mut file = std::fs::OpenOptions::new()
+    //             .create(true)
+    //             .append(true)
+    //             .custom_flags(0o0040000)
+    //             .open(&format!("seqnum-{}.out", i))
+    //             .unwrap();
+    //         let t = Instant::now();
+    //         let n = 1000000;
+    //         for i in 0..n {
+    //             file.write(format!("{:022}\n", i).as_bytes());
+    //         }
+    //         println!("avg: {:?}", t.elapsed() / n);
+    //     }).join().unwrap();
     // }
-    // println!("avg: {:?}", t.elapsed() / n);
-    let t = Instant::now();
-    let n = 1000000;
-    for i in 0..n {
-        new_seq_num(i, i + 2, i).await;
+    let tls = Arc::new(ThreadLocal::new());
+    let n = 10;
+    let m = 24;
+    for r in 0..m {
+        let tls = tls.clone();
+        tokio::spawn(async move {
+            let t = Instant::now();
+            for i in 0..n {
+                new_seq_num(&tls, i, i + 2, i).await;
+            }
+            println!("avg{}: {:?}", r, t.elapsed() / n as u32);
+        });
     }
-    println!("avg: {:?}", t.elapsed() / n as u32);
-    let mut map = AHashMap::new();
-    let mut file = std::fs::OpenOptions::new()
-        .read(true)
-        .open("seqnum.out")
-        .unwrap();
-    let mut buf = [0u8; 24];
-    loop {
-        let res = file.read_exact(&mut buf[..]);
-        if res.is_err() {
-            break;
-        }
-        let user_id1 = BigEndian::read_u64(&buf[0..8]);
-        let user_id2 = BigEndian::read_u64(&buf[8..16]);
-        let seq_num = BigEndian::read_u64(&buf[16..24]);
-        let key: u128 = ((user_id1 as u128) << 64) + user_id2 as u128;
-        map.insert(key, seq_num);
-    }
-    println!("{}", map.len());
+    tokio::time::sleep(Duration::from_secs(5)).await;
 }
--- server/seqnum/src/persistance/mod.rs
+++ server/seqnum/src/persistance/mod.rs
@@ -1,116 +1,103 @@
-use std::io::Write;
+use std::{
+    cell::RefCell,
+    sync::{atomic::AtomicU16, Arc},
+};
 
 use byteorder::{BigEndian, ByteOrder};
+use lazy_static::lazy_static;
+use thread_local::ThreadLocal;
+
 use lib::Result;
-#[allow(unused)]
-use tokio::io::AsyncWriteExt;
+
+pub(self) const MAX_FILE_SIZE: u64 = 1024 * 1024 * 48;
+
+lazy_static! {
+    static ref ID: AtomicU16 = AtomicU16::new(0);
+}
 
 #[cfg(not(feature = "tokio_append"))]
-static mut APPEND_FILE: Option<std::fs::File> = None;
+pub(crate) async fn new_seq_num(
+    file_thread_local: &Arc<ThreadLocal<RefCell<Option<std::fs::File>>>>,
+    signal_thread_local: &Arc<ThreadLocal<RefCell<tokio::sync::oneshot::Receiver<std::fs::File>>>>,
+    user_id: u64,
+    peer_id: u64,
+    seq_num: u64,
+) -> Result<()> {
+    use std::{io::Write, os::unix::prelude::OpenOptionsExt, sync::atomic::Ordering};
 
-#[cfg(feature = "tokio_append")]
-static mut APPEND_FILE: Option<tokio::fs::File> = None;
-
-pub(crate) async fn new_seq_num(user_id: u64, peer_id: u64, seq_num: u64) -> Result<()> {
     let (user_id1, user_id2) = if user_id < peer_id {
         (user_id, peer_id)
     } else {
         (peer_id, user_id)
     };
     let mut buf = [0u8; 24];
-    #[cfg(not(feature = "tokio_append"))]
-    unsafe {
-        match APPEND_FILE {
-            Some(ref mut file) => {
-                BigEndian::write_u64(&mut buf[0..8], user_id1);
-                BigEndian::write_u64(&mut buf[8..16], user_id2);
-                BigEndian::write_u64(&mut buf[16..24], seq_num);
-                let mut index = 0;
-                loop {
-                    match file.write(&buf[index..]) {
-                        Ok(size) => {
-                            if index + size == buf.len() {
-                                break;
-                            } else {
-                                index += size;
-                            }
-                        }
-                        Err(e) => {
-                            // just panic
-                            panic!("write error: {}", e);
-                        }
-                    }
-                }
-                Ok(())
-            }
-            None => {
-                let mut file = std::fs::OpenOptions::new()
-                    .create(true)
-                    .append(true)
-                    // todo file location set
-                    .open("seqnum.out")?;
-                BigEndian::write_u64(&mut buf[0..8], user_id1);
-                BigEndian::write_u64(&mut buf[8..16], user_id2);
-                BigEndian::write_u64(&mut buf[16..24], seq_num);
-                let mut index = 0;
-                loop {
-                    match file.write(&buf[index..]) {
-                        Ok(size) => {
-                            if index + size == buf.len() {
-                                break;
-                            } else {
-                                index += size;
-                            }
-                        }
-                        Err(e) => {
-                            // just panic
-                            panic!("write error: {}", e);
-                        }
-                    }
-                }
-                APPEND_FILE = Some(file);
-                Ok(())
-            }
+    BigEndian::write_u64(&mut buf[0..8], user_id1);
+    BigEndian::write_u64(&mut buf[8..16], user_id2);
+    BigEndian::write_u64(&mut buf[16..24], seq_num);
+    let file = file_thread_local.get_or(|| {
+        RefCell::new(Some(
+            std::fs::OpenOptions::new()
+                .create(true)
+                .append(true)
+                // todo value set on macos, windows
+                .custom_flags(0x4000)
+                // todo file location set
+                .open(&format!("seqnum-{}.out", ID.fetch_add(1, Ordering::AcqRel)))
+                .unwrap(),
+        ))
+    });
+    let file_option = &mut *file.borrow_mut();
+    let mut file = file_option.as_mut().unwrap();
+    let signal = signal_thread_local.get();
+    if let Some(signal) = signal {
+        let signal = &mut *signal.borrow_mut();
+        if let Ok(new_file) = signal.try_recv() {
+            let seqnum_file = std::fs::OpenOptions::new()
+                .create(true)
+                .append(true)
+                .custom_flags(0x4000)
+                .open(&format!(
+                    "seqnum-{}.out",
+                    ID.fetch_add(1, Ordering::AcqRel)
+                ))?;
+            let temp_file = file_option.take().unwrap();
+            std::fs::remove_file()
         }
-    }
-    #[cfg(feature = "tokio_append")]
-    unsafe {
-        match APPEND_FILE {
-            Some(ref mut file) => {
-                BigEndian::write_u64(&mut buf[0..8], user_id1);
-                BigEndian::write_u64(&mut buf[8..16], user_id2);
-                BigEndian::write_u64(&mut buf[16..24], seq_num);
-                match file.write_all(&buf[..]).await {
-                    Ok(_) => {}
+    } else {
+        if file.metadata()?.len() > MAX_FILE_SIZE {
+            let temp_file = std::fs::OpenOptions::new()
+                .create(true)
+                .append(true)
+                .custom_flags(0x4000)
+                .open(&format!(
+                    "seqnum-temp-{}.out",
+                    ID.fetch_add(1, Ordering::AcqRel)
+                ))?;
+            let old_file = file_option.take().unwrap();
+            file_option.replace(temp_file);
+            let (send_signal, recv_signal) = tokio::sync::oneshot::channel();
+            signal_thread_local.get_or(|| RefCell::new(recv_signal));
+            tokio::spawn(async move {});
+            file = file_option.as_mut().unwrap();
+        }
+        let mut index = 0;
+            loop {
+                match file.write(&buf[index..]) {
+                    Ok(size) => {
+                        if index + size == buf.len() {
+                            break;
+                        } else {
+                            index += size;
+                        }
+                    }
                     Err(e) => {
                         // just panic
                         panic!("write error: {}", e);
                     }
                 }
-                Ok(())
             }
-            None => {
-                let mut file = tokio::fs::OpenOptions::new()
-                    .create(true)
-                    .append(true)
-                    // todo file location set
-                    .open("seqnum")
-                    .await?;
-                BigEndian::write_u64(&mut buf[0..8], user_id1);
-                BigEndian::write_u64(&mut buf[8..16], user_id2);
-                BigEndian::write_u64(&mut buf[16..24], seq_num);
-                match file.write_all(&buf[..]).await {
-                    Ok(_) => {}
-                    Err(e) => {
-                        // just panic
-                        panic!("write error: {}", e);
-                    }
-                }
-                APPEND_FILE = Some(file);
-                Ok(())
-            }
-        }
     }
+    Ok(())
 }
 
 pub(crate) fn persistance_seq_num_start() -> Result<()> {
