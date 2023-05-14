3a738eafa4c178a78cb10add64af57c771cc57ad
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-05-15 00:16:33 +0800

    增加seq_num

--- server/seqnum/src/main.rs
+++ server/seqnum/src/main.rs
@@ -1,7 +1,7 @@
-use std::{sync::Arc, println, time::Duration};
+use std::{sync::Arc, time::Duration};
+
 use persistence::persistence_sequence_number_threshold;
 use thread_local::ThreadLocal;
-use tokio::io::AsyncReadExt;
 use tracing::Level;
 
 mod config;
@@ -22,23 +22,8 @@ async fn main() {
         .try_init()
         .unwrap();
     let file_tl = Arc::new(ThreadLocal::new());
-    for i in 0..33 {
+    for i in 0..5000 {
         _ = persistence_sequence_number_threshold(&file_tl, i % 3, i % 3 + 1, i).await;
     }
     tokio::time::sleep(Duration::from_secs(5)).await;
-    println!("ok");
-    let mut buf = [0u8; 24];
-    let mut dir = std::fs::read_dir("./").unwrap();
-    let file = dir.find(|e| {
-        let f = e.as_ref().unwrap();
-        f.file_name().to_str().unwrap().starts_with("seqnum-")
-    });
-    println!("{:?}", file);
-    if let Some(file) = file {
-        let file = file.unwrap();
-        let mut file = tokio::fs::File::open(file.path()).await.unwrap();
-        while let Ok(_) = file.read_exact(&mut buf).await {
-            println!("{:?}", buf);
-        }
-    }
 }
