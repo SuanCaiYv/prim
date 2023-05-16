fd7a22212ecb3a5c675006da18d11967a9846e9f
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-05-17 00:27:25 +0800

    增加net实现

--- server/lib/src/net/client_reqwest.rs
+++ server/lib/src/net/client_reqwest.rs
@@ -1,6 +1,6 @@
 use std::{
     sync::{
-        atomic::{AtomicU64, AtomicUsize, Ordering},
+        atomic::{AtomicU64, Ordering},
         Arc,
     },
     task::Waker,
@@ -16,7 +16,7 @@ use tracing::{debug, error};
 
 use crate::{entity::ReqwestMsg, net::ReqwestMsgIOUtil, Result};
 
-use super::{client::ClientConfig, ALPN_PRIM};
+use super::{client::ClientConfig, ReqwestHandlerGenerator, ALPN_PRIM};
 
 pub struct ReqwestClient {
     config: Option<ClientConfig>,
@@ -26,9 +26,11 @@ pub struct ReqwestClient {
 }
 
 pub(self) struct Operator(
-    AtomicU64,
-    mpsc::Sender<(u64, ReqwestMsg, oneshot::Sender<Result<ReqwestMsg>>, Waker)>,
     u16,
+    mpsc::Sender<(
+        ReqwestMsg,
+        Option<(u64, oneshot::Sender<Result<ReqwestMsg>>, Waker)>,
+    )>,
 );
 
 pub struct Reqwest {
@@ -37,10 +39,8 @@ pub struct Reqwest {
     req: Option<ReqwestMsg>,
     operator_sender: Option<
         tokio::sync::mpsc::Sender<(
-            u64,
             ReqwestMsg,
-            tokio::sync::oneshot::Sender<Result<ReqwestMsg>>,
-            Waker,
+            Option<(u64, tokio::sync::oneshot::Sender<Result<ReqwestMsg>>, Waker)>,
         )>,
     >,
     sender_task: Option<BoxFuture<'static, Result<()>>>,
@@ -48,15 +48,14 @@ pub struct Reqwest {
 }
 
 pub struct ReqwestState {
-    index: AtomicUsize,
+    req_id: AtomicU64,
     operator_list: Vec<Operator>,
 }
 
 impl ReqwestState {
     pub fn call(&self, mut req: ReqwestMsg) -> Reqwest {
-        let index = self.index.fetch_add(1, Ordering::SeqCst);
-        let operator = &self.operator_list[index % self.operator_list.len()];
-        let req_id = operator.0.fetch_add(1, Ordering::SeqCst);
+        let req_id = self.req_id.fetch_add(1, Ordering::SeqCst);
+        let operator = &self.operator_list[(req_id as usize) % self.operator_list.len()];
         let req_sender = operator.1.clone();
         req.set_req_id(req_id);
         Reqwest {
@@ -80,7 +79,7 @@ impl ReqwestClient {
         }
     }
 
-    pub async fn build(&mut self) -> Result<ReqwestState> {
+    pub async fn build(&mut self, generator: Arc<ReqwestHandlerGenerator>) -> Result<ReqwestState> {
         let ClientConfig {
             remote_address,
             ipv4_type,
@@ -117,11 +116,12 @@ impl ReqwestClient {
         let mut operator_list = Vec::with_capacity(max_bi_streams as usize);
 
         for i in 0..max_bi_streams {
-            let req_id = AtomicU64::new(0);
-            let (sender, mut receiver) = tokio::sync::mpsc::channel::<(
+            let (sender, mut receiver) = mpsc::channel::<(
                 ReqwestMsg,
                 Option<(u64, oneshot::Sender<Result<ReqwestMsg>>, Waker)>,
-            )>(1024);
+            )>(16384);
+            let (msg_sender_outer, msg_receiver_outer) = mpsc::channel(16384);
+            let (msg_sender_inner, mut msg_receiver_inner) = mpsc::channel(16384);
             let (mut send_stream, mut recv_stream) = match connection.open_bi().await {
                 Ok(v) => v,
                 Err(e) => {
@@ -135,6 +135,7 @@ impl ReqwestClient {
             let (tx, mut rx) = mpsc::channel::<u64>(4096);
             let stream_id = recv_stream.id().0;
             let timeout = self.req_timeout;
+            let sender_clone = sender.clone();
 
             tokio::spawn(async move {
                 let resp_sender_map = resp_sender_map0.clone();
@@ -143,20 +144,24 @@ impl ReqwestClient {
                 let task1 = async {
                     loop {
                         match receiver.recv().await {
-                            Some((req_id, req, sender, waker)) => {
-                                resp_sender_map.insert(req_id, sender);
-                                waker_map.insert(req_id, waker);
-                                let res = ReqwestMsgIOUtil::send_msg(&req, &mut send_stream).await;
-                                let tx = tx.clone();
-                                tokio::spawn(async move {
-                                    tokio::time::sleep(timeout).await;
-                                    _ = tx.send(req_id).await;
-                                });
-                                if let Err(e) = res {
-                                    error!("send msg error: {}", e.to_string());
-                                    break;
+                            Some((req, external)) => match external {
+                                Some((req_id, sender, waker)) => {
+                                    resp_sender_map.insert(req_id, sender);
+                                    waker_map.insert(req_id, waker);
+                                    let res =
+                                        ReqwestMsgIOUtil::send_msg(&req, &mut send_stream).await;
+                                    let tx = tx.clone();
+                                    tokio::spawn(async move {
+                                        tokio::time::sleep(timeout).await;
+                                        _ = tx.send(req_id).await;
+                                    });
+                                    if let Err(e) = res {
+                                        error!("send msg error: {}", e.to_string());
+                                        break;
+                                    }
                                 }
-                            }
+                                None => {}
+                            },
                             None => {
                                 debug!("receiver closed.");
                                 _ = send_stream.finish().await;
@@ -173,22 +178,26 @@ impl ReqwestClient {
                 let task2 = async {
                     loop {
                         match ReqwestMsgIOUtil::recv_msg(&mut recv_stream, None).await {
-                            Ok(resp) => {
-                                let req_id = resp.req_id();
-                                match resp_sender_map.remove(&req_id) {
-                                    Some(sender) => {
-                                        _ = sender.1.send(Ok(resp));
+                            Ok(msg) => {
+                                let req_id = msg.req_id();
+                                if req_id ^ 0xF000_0000_0000_0000 == 0 {
+                                    msg_sender_outer.send(msg).await;
+                                } else {
+                                    match resp_sender_map.remove(&req_id) {
+                                        Some(sender) => {
+                                            _ = sender.1.send(Ok(msg));
+                                        }
+                                        None => {
+                                            error!("req_id: {} not found.", req_id)
+                                        }
                                     }
-                                    None => {
-                                        error!("req_id: {} not found.", req_id)
-                                    }
-                                }
-                                match waker_map.remove(&req_id) {
-                                    Some(waker) => {
-                                        waker.1.wake();
-                                    }
-                                    None => {
-                                        error!("req_id: {} not found.", req_id)
+                                    match waker_map.remove(&req_id) {
+                                        Some(waker) => {
+                                            waker.1.wake();
+                                        }
+                                        None => {
+                                            error!("req_id: {} not found.", req_id)
+                                        }
                                     }
                                 }
                             }
@@ -235,25 +244,57 @@ impl ReqwestClient {
                 }
                 .fuse();
 
-                pin_mut!(task1, task2, task3);
+                let task4 = async {
+                    loop {
+                        match msg_receiver_inner.recv().await {
+                            Some(msg) => {
+                                let res = sender_clone.send((msg, None)).await;
+                                if let Err(e) = res {
+                                    error!("send msg error: {}", e.to_string());
+                                    break;
+                                }
+                            }
+                            None => {
+                                debug!("msg_receiver_inner closed.");
+                                break;
+                            }
+                        }
+                    }
+                }
+                .fuse();
+
+                pin_mut!(task1, task2, task3, task4);
 
                 loop {
                     futures::select! {
                         _ = task1 => {},
                         _ = task2 => {},
                         _ = task3 => {},
+                        _ = task4 => {},
                         complete => {
                             break;
                         }
                     }
                 }
             });
-            operator_list.push(Operator(req_id, sender, i as u16));
+
+            let mut handler = generator();
+            tokio::spawn(async move {
+                handler
+                    .handle((msg_sender_inner, msg_receiver_outer))
+                    .await
+                    .map_err(|e| {
+                        error!("handler error: {}", e.to_string());
+                        e
+                    })?;
+                Result::<()>::Ok(())
+            });
+            operator_list.push(Operator(i as u16, sender));
         }
         self.endpoint = Some(endpoint);
         self.connection = Some(connection);
         Ok(ReqwestState {
-            index: AtomicUsize::new(0),
+            req_id: AtomicU64::new(0),
             operator_list,
         })
     }
