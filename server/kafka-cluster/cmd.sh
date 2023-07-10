3ee06556967d294c681d3ba8b3588c16bcfdcc40
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-07-11 00:00:46 +0800

    增加了kafka配置

--- server/kafka-cluster/cmd.sh
+++ server/kafka-cluster/cmd.sh
@@ -1,3 +1,27 @@
-bin/kafka-server-start.sh config/server1.properties
-bin/kafka-server-start.sh config/server2.properties
-bin/kafka-server-start.sh config/server3.properties
+#!/bin/bash
+
+EXECUTE_CMD="$HOME/kafka"
+CONFIG_DIR=$(dirname "$PWD")/kafka-cluster
+
+echo $CONFIG_DIR
+
+case $1 in
+    "start") {
+        KAFKA_CLUSTER_ID=$($EXECUTE_CMD/bin/kafka-storage.sh random-uuid)
+        for i in {1..3}
+        do
+            echo "-------------starting kafka-$i-------------"
+            "$EXECUTE_CMD/bin/kafka-server-start.sh -daemon $CONFIG_DIR/server$i.properties"
+            # before first run, you should back commont the next line
+            "$EXECUTE_CMD/bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c $CONFIG_DIR/server$i.properties"
+            # $EXECUTE_CMD/bin/kafka-server-start.sh -daemon $CONFIG_DIR/server$i.properties
+        done
+    };;
+    "stop") {
+        for i in {1..3}
+        do
+            echo "-------------stopping kafka-$i-------------"
+            $EXECUTE_CMD/bin/kafka-server-stop.sh
+        done
+    };;
+    esac
